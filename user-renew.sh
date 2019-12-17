5eabbfd68fd354b300a061c7aa4967b07e902000
xxnnxxnn <58142522+xxnnxxnn@users.noreply.github.com>
2019-12-17 13:17:24 +0200

    Update user-renew.sh

--- user-renew.sh
+++ user-renew.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-#Script Perpanjang User SSH
+#Script sukurti Useri SSH
 
 read -p "Username : " Login
 read -p "Password Baru : " Pass
-read -p "Penambahan Masa Aktif (hari): " masaaktif
+read -p "Penambahan Masa Aktif (skaicius): " masaaktif
 userdel $Login
 useradd -e `date -d "$masaaktif days" +"%Y-%m-%d"` -s /bin/false -M $Login
 exp="$(chage -l $Login | grep "Account expires" | awk -F": " '{print $2}')"
 echo -e "$Pass\n$Pass\n"|passwd $Login &> /dev/null
 echo -e "--------------------------------"
 
-echo -e "Akun Sudah Diperpanjang Hingga $exp"
-echo -e "Password telah diganti dengan $Pass"
+echo -e "Paskyra buvo pratęsta iki $exp"
+echo -e "Slaptažodis buvo pakeistas $Pass"
 echo -e "==========================="
