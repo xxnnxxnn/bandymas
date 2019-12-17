9e2d6aee0c3bd9ec2d66818524056c142f9a0c0e
xxnnxxnn <58142522+xxnnxxnn@users.noreply.github.com>
2019-12-17 13:12:13 +0200

    Update create-user.sh

--- create-user.sh
+++ create-user.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#Script auto create user SSH
+#Script kurti userems SSH
 
 read -p "Username : " Login
 read -p "Password : " Pass
@@ -10,12 +10,12 @@ useradd -e `date -d "$masaaktif days" +"%Y-%m-%d"` -s /bin/false -M $Login
 exp="$(chage -l $Login | grep "Account expires" | awk -F": " '{print $2}')"
 echo -e "$Pass\n$Pass\n"|passwd $Login &> /dev/null
 echo -e ""
-echo -e "Informasi SSH"
-echo -e "=========-account-=========="
+echo -e "Informacija SSH"
+echo -e "=========-akauntas-=========="
 echo -e "Host: $IP" 
-echo -e "Port: 22, 80, 143, 443, 109, 110"
+echo -e "Port: 22, 80, 8080"
 echo -e "Username: $Login "
 echo -e "Password: $Pass"
 echo -e "-----------------------------"
-echo -e "Aktif Sampai: $exp"
+echo -e "Galiojimo data: $exp"
 echo -e "==========================="
